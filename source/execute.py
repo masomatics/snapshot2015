5d3138898983da3d143c05e5f35b6c765efbbb73
masomatics <koyama.masanori@gmail.com>
2015-09-25 11:54:47 +0900

    A little success on the execute.py if there had been no typo

--- source/execute.py
+++ source/execute.py
@@ -1,7 +1,20 @@
 import numpy as np
 import  main
 reload(main)
+import timeit
+
 
 theta_approx0 = np.array([-1.5, 0, 0, 0.1, 0.2])
-test_seq = main.DM_test(Nx_obs = 1000, Nx_test = 20000, alpha =0.1, tend = 4, theta_init= theta_approx0)
+nxobs = 1000
+nxtest = 20000
+alpha0 = 0.1
+t_end = 15
+
+
+test_seq = main.DM_test(Nx_obs = nxobs, Nx_test = nxtest, alpha =alpha0, tend = t_end, theta_init= theta_approx0)
+
+
+start = timeit.timeit()
 test_seq.run(200)
+end = timeit.timeit()
+print end - start
