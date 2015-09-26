34d1194ab69c3ee962c1adb791cc4b2c8fb7c7aa
masomatics <koyama.masanori@gmail.com>
2015-09-26 10:10:14 +0900

    Forgot to commit at the top level

--- source/execute.py
+++ source/execute.py
@@ -5,14 +5,14 @@ from timeit import default_timer as timer
 
 
 theta_approx0 = np.array([-1.5, 0, 0, 0.1, 0.2])
-#theta_approx0 = np.array( [ -2.06345867,  -0.40571014, -26.41343345 ,  6.47108246 ,  0.2       ])
+#theta_approx0 = np.array( [ -0.77172504,  -0.48468371, -25.73231738,   6.42606596,   0.2       ])
 nxobs = 1000
 nxtest = 20000
-alpha0 = 0.1
-t_end = 40
-n_iter = 2000
+alpha0 = 0.01
+t_end = 12
+n_iter = 4000
 
 test_seq = main.DM_test(Nx_obs = nxobs, Nx_test = nxtest, alpha =alpha0, tend = t_end, theta_init= theta_approx0)
 
 
-test_seq.run(n_iter,write = False)
+test_seq.run(n_iter,write = True)
