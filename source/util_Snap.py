fix fatal error in util.distances; axis of average was opposite Waros...

source/util_Snap.py
@@ -23,8 +23,8 @@ def convert_hist_to_scatter(myhist):
 
 
 def distances(dat):
-    datmean = np.mean(dat, axis =0)
-    centered = dat - datmean
+    datmean = np.mean(dat, axis =1)
+    centered = np.transpose(dat) - datmean
     centeredsqr = centered**2
     centeredL2  = np.sqrt(np.sum(centeredsqr, axis = 1))
     return centeredL2

source/variance_test_normal.py
@@ -14,7 +14,7 @@ import matplotlib.pyplot as plt
 import datetime
 import matplotlib.animation as manimation
 
-dimdat = 400
+dimdat = 50
 N = 50   #obs
 M = 100       #sim
 numexp = 200
@@ -30,7 +30,9 @@ loc1 = 0.5
 loc2 = 0.
 alphas = np.linspace(0,1., 101)
 graphlim = 1
-myxlim = 70
+myxlim = np.sqrt(dimdat)*2
+myattention_index = [0]
+
 datelocation = "../records"
 
 
@@ -50,7 +52,6 @@ datobs = np.random.multivariate_normal(mean = np.transpose(mu1)[0], cov = sig1, 
 
 
 resoln = 500
-myattention_index = [0]
 
 
 